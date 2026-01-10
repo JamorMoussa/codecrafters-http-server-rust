ed130197bb210cdf5abf63b04d130dd888aac113
JamorMoussa <moussajamorsup@gmail.com>
2026-01-10 20:48:38 +0100

    handle paths

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use std::{ffi::CStr, io::Write};
+use std::{collections::HashMap, io::{Read, Write}};
 #[allow(unused_imports)]
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 use log::{debug, error, info};
 
 const CRLF: &str = "\r\n";
 
+struct Headers {
+    headers: HashMap<String, String>
+}
+
 struct HTTPResponse {
-    http_version: String,
     status_code: i32,
     reason: String,
 }
@@ -14,13 +17,11 @@ struct HTTPResponse {
 impl HTTPResponse {
 
     fn new(
-        http_version: &str,
         status_code: i32,
         reason: &str
     ) -> Self {
 
         HTTPResponse {
-            http_version: http_version.to_string(),
             status_code: status_code,
             reason: reason.to_string(),
         }
@@ -28,19 +29,51 @@ impl HTTPResponse {
 
     fn as_bytes(self) -> Vec<u8>{
         
-        let HTTPResponse {http_version, status_code, reason} = self;
+        let HTTPResponse {status_code, reason} = self;
 
         let response = format!(
-            "{} {} {}{}{}",
-            http_version,
-            status_code, 
-            reason, CRLF, CRLF
+            "HTTP/1.1 {} {}{}{}",
+            status_code, reason, CRLF, CRLF
         );
 
         response.into_bytes()
     }
 }
 
+
+struct HTTPRequest{
+    method: String,
+    path: String,
+    http_version: String,
+    headers: Headers
+}
+
+impl HTTPRequest {
+
+    fn parse(stream: &mut TcpStream) -> Self{
+
+        let mut buf = String::new();
+
+        let _ = stream.read_to_string(&mut buf);
+
+        let request = buf.split(CRLF).collect::<Vec<&str>>();
+
+        // request line 
+        let request_line = request[0].split_whitespace().collect::<Vec<&str>>();
+
+        let method = request_line[0].to_string();
+        let path = request_line[1].to_string();
+        let http_version = request_line[2].to_string();
+
+        HTTPRequest {
+            method: method,
+            path: path,
+            http_version: http_version,
+            headers: Headers { headers: HashMap::new()}
+        }
+
+    }
+}
 
 
 fn main() {
@@ -56,11 +89,21 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 info!("accepted new connection");
+
+                let request = HTTPRequest::parse(&mut stream);
+
+                let (mut status_code, mut reason) = (200, "OK");
+
+                debug!("{}",request.path);
+
+                if request.path != "/index.html" {
+                    (status_code, reason) = (404, "Not Found");
+                    error!("Not Found");
+                }
                 
-                let mut response = HTTPResponse::new(
-                    "HTTP/1.1", 200, "OK",
-                );
-                stream.write(&mut response.as_bytes());
+                let response = HTTPResponse::new(status_code, reason);
+                
+                let _ = stream.write(&response.as_bytes());
             }
             Err(e) => {
                 error!("error: {}", e);
